e644d9642fb5bbf528ccdfe34018995ac856181c
Scottt <scott.geyer@eagles.usm.edu>
2017-04-16 01:49:09 -0500

    Update loadStuff.js

--- script/loadStuff.js
+++ script/loadStuff.js
@@ -244,6 +244,8 @@ function nextQuestion() {
 		up[1] = up[2];
 		up[2] = up[3];
 		up[3] = document.getElementById('then2').value;
+		document.getElementById('then2').value = up[3] + " successfully added.";
+		log += up[3] + ", manually added";
 		
 
 		n = dynamicClass.length;
